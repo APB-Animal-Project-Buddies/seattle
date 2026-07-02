01e17750d4e2ee4c8d4324defe3df0532748b03a
vpamrit <vishnuamritpydah@gmail.com>
2026-07-02 14:08:10 -0700

    chore: remove local nhost/ folder (use backend_migrations instead)

--- nhost/migrations/default/1782600000000_add_review_instance_active/down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS review_instance_author_active_idx;
-ALTER TABLE review_instance DROP COLUMN IF EXISTS active_until;
-ALTER TABLE review_instance DROP COLUMN IF EXISTS author_id;
